195671c9448571b958b5ac9a6412e214026d0d15
Stephane Mansour <stephane.mansour@gmail.com>
2017-06-06 16:35:41 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :videos, only: [:index, :show, :new, :create, :destroy]
+  resources :applications
+
+  namespace :business do
+    resources :videos, only: [:index, :show, :new, :create, :destroy]
+    resources :postings
+  end
 end
