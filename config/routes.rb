94a9ea36864cfcd26e45eca4a41266ad7117fcb1
Stephane Mansour <stephane.mansour@gmail.com>
2017-06-06 19:15:17 +0100

    routes improved

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :videos, only: [:index, :show, :new, :create, :destroy]
-  resources :applications
+  resources :postings, only: [:index, :show] do
+    resources :applications, only: [:new, :create]
+  end
+
+  resources :applications, only: [:index, :show, :edit, :update, :destroy]
 
   namespace :business do
     resources :videos, only: [:index, :show, :new, :create, :destroy]
-    resources :postings
+    resources :postings, only: [:new, :create, :edit, :update, :destroy]
   end
 end
