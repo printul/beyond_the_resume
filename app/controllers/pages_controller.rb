471874beed2b5f9dceb10282a5eade7794c5f7e4
Hongshu Liu <liuhongshu@gmail.com>
2017-06-08 20:01:13 +0100

    user dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if user_signed_in?
+      if current_user.business?
+        redirect_to businesses_path
+      else
+      redirect_to user_path(current_user)
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users,
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+
   root to: 'pages#home'
+  resources :users, only: [:show]
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :videos, only: [:index, :show, :new, :create, :destroy]
   resources :postings, only: [:index, :show] do
     resources :applications, only: [:new, :create]
