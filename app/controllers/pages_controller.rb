9422f95642672e205dd093f3214a3f506bc661ae
Hongshu Liu <liuhongshu@gmail.com>
2017-06-09 13:36:19 +0100

    changes made due to schema

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 
   def home
     if user_signed_in?
-      if current_user.business?
+      if current_user.is_business?
         redirect_to businesses_path
       else
       redirect_to user_path(current_user)
